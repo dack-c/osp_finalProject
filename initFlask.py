9ec9f156f3c15f8c4db5fab549f04d4062792f65
dack-c <qpxmaos3845@gmail.com>
2020-06-24 19:33:53 +0900

    2nd modify index.html and initFlask.py for debuging

--- initFlask.py
+++ initFlask.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from Single_website_Crawler import URLData, analyze_URL_words, jsonify_URLData
+import pdb
 
 app = Flask(__name__)
 
@@ -16,14 +17,16 @@ def startPage():
 def singleURL_processing_page():
     if (request.method == 'POST'):
         requestedURL = request.form['url'] #get singleURL from input Box
-        
+
         URL_res = URLData()
         try:
            URL_res = analyze_URL_words(requestedURL)
         except Exception as e: #URL 요청 실패
             print(e)
+            pdb.set_trace()
             return render_template('index.html', wordDictionary={}, succeed=False)
 
+    pdb.set_trace()
     return render_template('index.html', wordDictionary=jsonify_URLData(URL_res), succeed=True)
 
 
